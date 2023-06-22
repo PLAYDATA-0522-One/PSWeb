86a42c4c95b102999dad23a152fb2e419bdee7a9
Amy <wlgpek90@naver.com>
2023-06-22 13:27:42 +0900

    add aboutus

--- src/main/java/com/playdata/game/servlet/AboutUsServlet.java
+++ src/main/java/com/playdata/game/servlet/AboutUsServlet.java
@@ -4,15 +4,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class MainServlet extends HttpServlet {
+public class AboutUsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        String uname = (String) session.getAttribute("uname");
-        req.setAttribute("uname", uname);
-        req.getRequestDispatcher("views/main.jsp").forward(req, resp);
+        req.getRequestDispatcher("views/aboutus.html").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("views/aboutus.html").forward(req, resp);
     }
 }
